4afd50c92f9f085a0a49219ecba840fd9076f4bc
FreezyCGI <patrick.fritzl@gmx.at>
2023-12-16 15:57:27 +0100

    Finished Start sequenz

---
+++ IceCube/Assets/Minigame/StartSequenz/SequenzChanger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SequenzChanger : MonoBehaviour
+{
+    [SerializeField] 
+    TMP_Text sequenzText;
+
+    [SerializeField]
+    TriggerableBase FirstBuilding;
+
+    public void ChangeText(string text)
+    {
+        sequenzText.SetText(text);
+    }
+
+    public void StartMinigame()
+    {
+        ChangeText("GO!");
+        FirstBuilding.OnStart(true);
+    }
+}
