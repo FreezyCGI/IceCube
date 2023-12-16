9dc181d8369d55c5a8b45d18e506d0ef70a6bde9
lisa <lisa.gassinger@gmx.at>
2023-12-16 20:51:32 +0100

    uppsi

---
+++ IceCube/Assets/Crane/Crane.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crane : TriggerableBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public override void OnStart(bool success)
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+    public override void OnCanContinueToNextObject()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ContinueToNextObject(bool success)
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+   
+}
