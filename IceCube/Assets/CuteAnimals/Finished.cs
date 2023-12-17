979d09597bc8349cd894b27b8719241954f86031
FreezyCGI <patrick.fritzl@gmx.at>
2023-12-17 02:43:54 +0100

    Added Fireworks

--- IceCube/Assets/CuteAnimals/Finished.cs
+++ IceCube/Assets/CuteAnimals/Finished.cs
@@ -7,6 +7,8 @@ public class Finished : MonoBehaviour
     [SerializeField]
     GameObject CubeParent;
 
+    [SerializeField]
+    GameObject Fireworks;
 
     public void OnFinished()
     {
@@ -14,5 +16,6 @@ public class Finished : MonoBehaviour
         IceCube iceCube = FindFirstObjectByType<IceCube>();
         iceCube.transform.SetParent(CubeParent.transform);
         iceCube.transform.localPosition = Vector3.zero;
+        Fireworks.SetActive(true);
     }
 }
