6dacba7c50390a31369add655b402bb4df634d9f
FreezyCGI <patrick.fritzl@gmx.at>
2023-12-17 02:11:57 +0100

    Crane prototype finished

--- IceCube/Assets/CuteAnimals/Finished.cs
+++ IceCube/Assets/CuteAnimals/Finished.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Finished : MonoBehaviour
 {
+    [SerializeField]
+    GameObject CubeParent;
+
+
     public void OnFinished()
     {
         FindFirstObjectByType<MinigameManager>().ShowWon();
+        IceCube iceCube = FindFirstObjectByType<IceCube>();
+        iceCube.transform.SetParent(CubeParent.transform);
+        iceCube.transform.localPosition = Vector3.zero;
     }
 }
